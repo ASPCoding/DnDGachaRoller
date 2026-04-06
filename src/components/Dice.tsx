b880c85de252d647c23e776a3c24439dbcfd1bd8
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2026-04-05 18:03:53 -0700

    Created Secondary State for Dice

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,12 +1,36 @@
 import "./Dice.css"
 
+function RollingDice(){
+  return(
+    <svg id="die" viewBox="0 0 100 100">
+      <path id="hexagon"/>
+    </svg>
+  );
+}
+
+function StationaryDice(){
+  return(
+    <svg id="square" viewBox="0 0 100 100">
+      <path id="square-border"/>
+    </svg>
+  )
+}
+
+function CurrentDice(){
+  let currDice = (<div></div>);
+  if(true){
+    currDice = StationaryDice();
+  }else{
+    currDice = RollingDice();
+  }
+  return currDice;
+}
+
 export default function Dice(){
   return(
     <>
       <div id="die-wrapper">
-        <svg id="die" viewBox="0 0 100 100">
-          <path id="hexagon"/>
-        </svg>
+        <CurrentDice/>
 
         <div id="num-wrapper">
           <p>er?</p>
