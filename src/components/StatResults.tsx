cdd8c948798cbb5990b6bb9caa1a6e16e4a7952e
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2026-04-15 03:56:32 -0700

    Added StatResult Component

---
+++ src/components/StatResults.tsx
@@ -0,0 +1,34 @@
+import {useState, useEffect} from "react"
+import AppStore from "../stores/AppStore"
+import "./StatResults.css"
+
+export default function StatResults(){
+  const [results, setResults] = useState([0, 0, 0, 0, 0, 0])
+
+  function addResult(){
+    setTimeout(() => {
+      setResults([...AppStore.getStatResults()])
+    }, 5100)
+  }
+  
+  useEffect(() => {
+    AppStore.on("toggleRoll", addResult)
+
+    return () => {
+      AppStore.off("toggleRoll", addResult)
+    }
+
+  },[results])
+
+  return(
+    <>
+      <div id="stat-wrapper">
+        {results.map((item) =>
+          <div>
+            <h1>{item}</h1>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
